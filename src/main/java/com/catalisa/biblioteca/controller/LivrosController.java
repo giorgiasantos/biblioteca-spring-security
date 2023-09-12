eb3c3c8c0f60e51aa5f5de3118065a51f904b720
giorgiasantos <giorgia.santos@zup.com.br>
2023-09-12 10:41:25 -0300

    Adicionando response entities

--- src/main/java/com/catalisa/biblioteca/controller/LivrosController.java
+++ src/main/java/com/catalisa/biblioteca/controller/LivrosController.java
@@ -5,6 +5,7 @@ import com.catalisa.biblioteca.model.LivrosModel;
 import com.catalisa.biblioteca.service.LivrosService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
@@ -27,8 +28,12 @@ public class LivrosController {
     //pega uma lista e procura dentro dela uma informação
     @PreAuthorize("hasAnyRole('ROLE_USER', 'ROLE_ADMIN')")
     @GetMapping(path = "/livros/{id}")
-    public Optional<LivrosModel> buscaLivroPorId(@PathVariable Long id){
-        return livrosService.buscaLivrosPorId(id);
+    public ResponseEntity<?> buscaLivroPorId(@PathVariable Long id){
+        Optional<LivrosModel> livro = livrosService.buscaLivrosPorId(id);
+        if(livro.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Livro não encontrado!");
+        }
+        return ResponseEntity.ok().body(livro.get());
     }
 
     //Endpoints de INSERÇÃO
@@ -36,18 +41,19 @@ public class LivrosController {
     //Requisição POST - insere algum dado dentro do banco
     @PreAuthorize("hasAnyRole('ROLE_USER', 'ROLE_ADMIN')")
     @PostMapping(path = "/livros")
-    @ResponseStatus(HttpStatus.CREATED)
-    public LivrosModel cadastrarNovoLivro(@RequestBody LivrosModel livrosModel){ //colocamos o livrosModel como parametro para saber o que presiamos colocar no corpo da requisição
-        return livrosService.cadastrarLivro(livrosModel);
+    public ResponseEntity<?> cadastrarNovoLivro(@RequestBody LivrosModel livrosModel){ //colocamos o livrosModel como parametro para saber o que presiamos colocar no corpo da requisição
+
+        LivrosModel livro = livrosService.cadastrarLivro(livrosModel);
+        return ResponseEntity.ok().body("Novo livro cadastrado com sucesso! \n" + livro);
     }
 
     //Endpoint de ALTERAÇÃO
     //Requisição PUT - altera algum dado dentro do banco
     @PreAuthorize("hasAnyRole('ROLE_USER', 'ROLE_ADMIN')")
     @PutMapping(path = "/livros/{id}")
-    public LivrosModel alteraLivro(@PathVariable Long id,
-                                   @RequestBody LivrosModel livrosModel){
-        return livrosService.alterarLivro(id, livrosModel);
+    public ResponseEntity<LivrosModel> alteraLivro(@PathVariable Long id, @RequestBody LivrosModel livrosModel){
+        LivrosModel livro = livrosService.alterarLivro(id, livrosModel);
+        return ResponseEntity.ok().body(livro);
     }
 
     //Endpoint de deleção
--- src/main/java/com/catalisa/biblioteca/controller/UserController.java
+++ src/main/java/com/catalisa/biblioteca/controller/UserController.java
@@ -2,8 +2,10 @@ package com.catalisa.biblioteca.controller;
 
 import com.catalisa.biblioteca.model.UserModel;
 import com.catalisa.biblioteca.service.UserService;
+import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
@@ -24,21 +26,27 @@ public class UserController {
 
     @PreAuthorize("hasAnyRole('ROLE_USER', 'ROLE_ADMIN')")
     @GetMapping(path = "/{id}")
-    public Optional<UserModel> buscarUsuarioPorId(@PathVariable Long id){
-        return userService.buscarPorId(id);
+    public ResponseEntity<?> buscarUsuarioPorId(@PathVariable Long id){
+        Optional<UserModel> user = userService.buscarPorId(id);
+        if(user.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Usuário não encontrado.");
+        }
+        return ResponseEntity.ok(user.get());
     }
 
     @PreAuthorize("hasAnyRole('ROLE_USER', 'ROLE_ADMIN')")
     @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
-    public UserModel cadastrarUser(@RequestBody UserModel userModel){
-        return userService.cadastrarUser(userModel);
+    //@ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<?> cadastrarUser(@RequestBody UserModel userModel){
+        UserModel user = userService.cadastrarUser(userModel);
+        return ResponseEntity.ok().body("Novo usuário cadastrado com sucesso! \n" + user);
     }
 
     @PreAuthorize("hasAnyRole('ROLE_USER', 'ROLE_ADMIN')")
     @PutMapping(path = "/{id}")
-    public UserModel alterarUser (@PathVariable Long id, @RequestBody UserModel userModel){
-        return userService.alterarUser(id,userModel);
+    public ResponseEntity<UserModel> alterarUser (@PathVariable Long id, @RequestBody UserModel userModel){
+        UserModel user = userService.alterarUser(id,userModel);
+        return ResponseEntity.ok().body(user);
     }
 
     @PreAuthorize("hasAnyRole('ROLE_ADMIN')")
